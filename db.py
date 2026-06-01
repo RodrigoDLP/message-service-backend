505eeba32d169b8aea9a595ee43259231802d884
RodrigoDLP <rodrigo.delapuente@utec.edu.pe>
2026-06-01 16:49:09 -0500

    Delete db.py

--- db.py
+++
@@ -1,9 +0,0 @@
-import os
-from dotenv import load_dotenv
-import psycopg2
-
-load_dotenv()  # carga variables del archivo .env
-
-def get_connection():
-    db_url = os.getenv("DATABASE_URL")
-    return psycopg2.connect(db_url)
